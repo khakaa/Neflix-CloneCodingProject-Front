10f41a75ceb364041100806a34a75bafe0f179ce
erionRK621 <jhkim62134@naver.com>
2021-10-19 10:52:29 +0900

    feat: row 추가

--- netfilx/src/pages/Browse.js
+++ netfilx/src/pages/Browse.js
@@ -1,25 +1,70 @@
 import React from "react";
+import styled from "styled-components";
+import Row from "../components/Row";
 
 const Browse = () => {
   return (
-    <div>
-      <div>내용</div>
-      <div>내용</div>
-      <div>내용</div>
-      <div>내용</div>
-      <div>내용</div>
-      <div>내용</div>
-      <div>내용</div>
-      <div>내용</div>
-      <div>내용</div>
-      <div>내용</div>
-      <div>내용</div>
-      <div>내용</div>
-      <div>내용</div>
-      <div>내용</div>
-      <div>내용</div>
-    </div>
+    <React.Fragment>
+      <Wrap>
+        <Banner>
+          <BtnWrap>
+            <Button>재생</Button>
+            <Button>상세정보</Button>
+          </BtnWrap>
+        </Banner>
+
+        <Grid>
+          <Row />
+          <Row />
+          <Row />
+          <Row />
+          <Row />
+        </Grid>
+      </Wrap>
+    </React.Fragment>
   );
 };
+
+const Wrap = styled.div`
+  width: 100%;
+`;
+
+const Banner = styled.div`
+  width: auto;
+  height: 600px;
+  background-color: black;
+  position: relative;
+`;
+const Grid = styled.div`
+  width: auto;
+  height: auto;
+  background-color: black;
+  overflow-y: auto;
+`;
+
+const BtnWrap = styled.div`
+  position: absolute;
+  bottom: 100px;
+  left: 20px;
+`;
+
+const Button = styled.button`
+  width: 115px;
+  height: 45px;
+  border: none;
+  padding: 4px;
+  margin: 0px 12px;
+  font-size: 0.9rem;
+  outline: 0px;
+  border-radius: 4px;
+
+  text-align: center;
+  cursor: pointer;
+  color: white;
+  background-color: #6d6d6eb3;
+  &:hover {
+    background-color: #6d6d6e66;
+  }
+`;
 
 export default Browse;
