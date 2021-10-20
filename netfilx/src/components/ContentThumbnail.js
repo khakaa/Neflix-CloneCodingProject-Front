bd83c90a73d3b80c3604235e920ab1bf9eb05d04
박하린 <phr0210@naver.com>
2021-10-20 22:31:37 +0900

    edit: video 태그 추가중

--- netfilx/src/components/ContentThumbnail.js
+++ netfilx/src/components/ContentThumbnail.js
@@ -31,14 +31,35 @@ const ContentThumbnail = (props) => {
         <Container>
           <ThumbnailImg
             onClick={showModal}
-            src={posterPath}
-            // src="https://occ-0-993-325.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABSF78muRZ4z-sgV4tQp8vM1VSE2SnlLYl_ThhQXzxbWPz-008QtjCtSCnoGduPscCSIvEETZ8ovDKC3wzDI-6XcwI-Q.webp?r=916"
+            // src={posterPath}
+            src="https://occ-0-993-325.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABSF78muRZ4z-sgV4tQp8vM1VSE2SnlLYl_ThhQXzxbWPz-008QtjCtSCnoGduPscCSIvEETZ8ovDKC3wzDI-6XcwI-Q.webp?r=916"
           />
         </Container>
         <Modal
           visible={visible}
           closeModal={closeModal}
           posterPath={posterPath}
+          overview={overview}
+          firstData={firstData}
+          grade={grade}
+        />
+      </>
+    );
+  }
+
+  if (youtubePath) {
+    return (
+      <>
+        <Container>
+          <ThumbnailVideo
+            onClick={showModal}
+            // src={posterPath}
+            src="https://occ-0-993-325.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABSF78muRZ4z-sgV4tQp8vM1VSE2SnlLYl_ThhQXzxbWPz-008QtjCtSCnoGduPscCSIvEETZ8ovDKC3wzDI-6XcwI-Q.webp?r=916"
+          />
+        </Container>
+        <Modal
+          visible={visible}
+          closeModal={closeModal}
           overview={overview}
           firstData={firstData}
           grade={grade}
@@ -67,5 +88,7 @@ const ThumbnailImg = styled.img`
     display: absolute;
   }
 `;
+
+const ThumbnailVideo = styled.video``;
 
 export default ContentThumbnail;
